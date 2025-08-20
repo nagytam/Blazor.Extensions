04950e171e174f311294769c8cb1ac0049fe0158
Tamas Nagy <mail@tamas-nagy.net>
2025-08-20 08:02:16 +0200

    Review

--- Blazor.Extensions.Components.Core.Tests/Helpers/PeriodicTaskTests.cs
+++ Blazor.Extensions.Components.Core.Tests/Helpers/PeriodicTaskTests.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 using Blazor.Extensions.Components.Core.Helpers;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Blazor.Extensions.Components.Core.Tests.Helpers;
 
