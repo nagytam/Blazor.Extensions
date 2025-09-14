436dc48f839703ccc3e48eb1241aa34f7441e6d3
Tamas Nagy <mail@tamas-nagy.net>
2025-09-14 20:09:26 +0200

    Version

---
+++ Blazor.Extensions.Presentation/Components/SoftwareVersionComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Reflection;
+
+namespace Blazor.Extensions.Presentation.Components;
+
+public class SoftwareVersionComponent : ComponentBase
+{
+    protected static string Version
+    {
+        get
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var version = assembly.GetName().Version;
+            var softwareVersion = $"{version?.Major:0000}.{version?.Minor:00}.{version?.Build:00000}"; // .{version?.Revision:00}
+            return $"Version: {softwareVersion}";
+        }
+    }
+}
