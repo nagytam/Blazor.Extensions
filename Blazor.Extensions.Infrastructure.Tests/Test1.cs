3315ed3eed29b260187a6b3c4511431fcbd4b28c
Tamas Nagy <mail@tamas-nagy.net>
2025-09-14 16:23:56 +0200

    Unit tests

---
+++ Blazor.Extensions.Infrastructure.Tests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Blazor.Extensions.Infrastructure.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Blazor.Extensions.Presentation.Tests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Blazor.Extensions.Presentation.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
