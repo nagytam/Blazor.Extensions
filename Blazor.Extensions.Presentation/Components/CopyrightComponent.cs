06ed1fa8c57e1da542c8f63572ae74fb896e860b
Tamas Nagy <mail@tamas-nagy.net>
2025-09-14 20:58:34 +0200

    Product & Copyright

---
+++ Blazor.Extensions.Presentation/Components/CopyrightComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Blazor.Extensions.Presentation.Components;
+
+public class CopyrightComponent : ComponentBase
+{
+    protected static string Copyright
+    {
+        get
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var copyright = FileVersionInfo.GetVersionInfo(assembly.Location).LegalCopyright ?? string.Empty;
+            return copyright;
+        }
+    }
+}
---
+++ Blazor.Extensions.Presentation/Components/SoftwareProductComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Blazor.Extensions.Presentation.Components;
+
+public class SoftwareProductComponent : ComponentBase
+{
+    protected static string Product
+    {
+        get
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var productName = FileVersionInfo.GetVersionInfo(assembly.Location).ProductName ?? string.Empty;
+            return productName;
+        }
+    }
+}
